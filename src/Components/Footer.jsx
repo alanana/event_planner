038ef9e1e239507f386ddf1b34b10bc4353f03da
alanana <alanana2020@gmail.com>
2024-07-11 10:27:27 -0400

    Initial commit for the event planner app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () =>{
+    return (
+        <>
+        <footer>
+            <p><a href="#">Terms</a></p>
+            <p><a href="#">Privacy</a></p>
+            <p>&copy; EventPlanner 2024</p>
+        </footer>
+        </>
+    )
+}
+export default Footer
